97851f20aa577489097d72ca5ec26b262c610ae6
remi.lacroix <remi.lacroix@c5a249bd-0e1c-7e75-0f87-4f2b50c314a1>
2011-03-17 08:00:35 +0000

    Début d'ajout des classes et interfaces utiles pour l'heuristique VNS :  - structure de bases pour les classes VNS générique et VNS spécialisé pour le problème de la p-médiane.  - interface permettant de manipuler des voisinages à taille variable.  - modification de la classe VoisinagePMediane représentant un voisinage pour le problème de le p-médiane pour qu'elle implémente l'interface des voisinages à taille variable.

---
+++ trunk/src/pmediane/VNSPMediane.java
@@ -0,0 +1,13 @@
+package pmediane;
+
+import pne.VNS;
+
+/**
+ * 
+ * 
+ * @author Bruno Duisit, Rémi Lacroix, Marie Nivet
+ */
+public class VNSPMediane extends VNS<DataPMediane, SolutionPMediane>
+{
+
+}
---
+++ trunk/src/pne/VNS.java
@@ -0,0 +1,11 @@
+package pne;
+
+/**
+ * 
+ * 
+ * @author Bruno Duisit, Rémi Lacroix, Marie Nivet
+ */
+public abstract class VNS<Data, Solution> implements Heuristique<Data, Solution>
+{
+	
+}
