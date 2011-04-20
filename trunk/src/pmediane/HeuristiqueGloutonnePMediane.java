b3cd9e592b48c00d33fdef71f300661152105206
remi.lacroix <remi.lacroix@c5a249bd-0e1c-7e75-0f87-4f2b50c314a1>
2011-04-20 20:29:11 +0000

    Ajout du code pour la fonction objectif et l'heuristique gloutonne (qui marche pas mal du tout d'ailleurs).

--- trunk/src/pmediane/HeuristiqueGloutonnePMediane.java
+++ trunk/src/pmediane/HeuristiqueGloutonnePMediane.java
@@ -1,5 +1,7 @@
 package pmediane;
 
+import java.util.Random;
+
 import pne.Heuristique;
 
 /**
@@ -25,6 +27,88 @@ public class HeuristiqueGloutonnePMediane implements Heuristique<DataPMediane, S
 	 */
 	public SolutionPMediane calculerSolution(DataPMediane donnees)
 	{
-		return null;
+		int c, centre, meilleurCentre = 0, i;
+		int val, valTmp;
+		
+		int[] dispo = new int[donnees.getNbEntites()];
+		int[] centres = new int[donnees.getNbCentres()];
+		int[] affectations = null;
+		int[] affectationsPrec = new int[donnees.getNbEntites()];
+		int[] affectationsTmp = new int[donnees.getNbEntites()];
+		int[] affectationsSecondaires = null;
+		int[] affectationsSecondairesPrec = new int[donnees.getNbEntites()];
+		int[] affectationsSecondairesTmp = new int[donnees.getNbEntites()];
+		
+		for (i=0; i<dispo.length; i++)
+			dispo[i] = i;
+		
+		// On choisit pour commencer le centre qui minimise
+		// la somme des distances à toutes les entités.
+		val = Integer.MAX_VALUE;
+		for (centre=0; centre<dispo.length; centre++)
+		{
+			valTmp = 0;
+			
+			for (i=0; i<affectationsTmp.length; i++)
+				valTmp += donnees.getDistance(i, centre);
+
+			if (valTmp < val)
+			{
+				meilleurCentre = centre;
+				val = valTmp;
+			}
+		}
+		
+		for (i=0; i<affectationsTmp.length; i++)
+		{
+			affectationsPrec[i] = meilleurCentre;
+			affectationsSecondairesPrec[i] = -1;
+		}
+		dispo[meilleurCentre] = dispo[dispo.length - 1];
+		
+		// On ouvre ensuite les autres centres
+		for (int nbCentres=1; nbCentres<centres.length; nbCentres++)
+		{
+			val = Integer.MAX_VALUE;
+			
+			for (c=0; c<dispo.length-nbCentres; c++)
+			{
+				centre = dispo[c];
+				valTmp = 0;
+				
+				for (i=0; i<affectationsTmp.length; i++)
+				{
+					if (donnees.getDistance(i, centre) < donnees.getDistance(i, affectationsPrec[i]))
+					{
+						affectationsSecondairesTmp[i] = affectationsTmp[i];
+						affectationsTmp[i] = centre;
+					}
+					else
+					{
+						affectationsTmp[i] = affectationsPrec[i];
+						
+						if (affectationsSecondairesPrec[i] == -1
+							|| donnees.getDistance(i, centre) < donnees.getDistance(i, affectationsSecondairesPrec[i]))
+							affectationsSecondairesTmp[i] = centre;
+					}
+					
+					valTmp += donnees.getDistance(i, affectationsTmp[i]);
+				}
+				
+				if (valTmp < val)
+				{
+					meilleurCentre = centre;
+					val = valTmp;
+					affectations = affectationsTmp.clone();
+					affectationsSecondaires = affectationsSecondairesTmp.clone();
+				}
+			}
+			
+			dispo[meilleurCentre] = dispo[dispo.length - nbCentres - 1];
+			affectationsPrec = affectations.clone();
+			affectationsSecondairesPrec = affectationsSecondaires.clone();
+		}
+		
+		return new SolutionPMediane(donnees, centres, affectations, affectationsSecondaires);
 	}
 }
